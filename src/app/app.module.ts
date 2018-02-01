fe8641c08f433b59050c8eb337800ced4e911aa3
Vinicius Melo <Melo@MacBook-Air-de-USUARIO.local>
2018-02-01 16:44:29 -0200

    ïtegracao nfc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { NFC, Ndef } from '@ionic-native/nfc';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -34,7 +36,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    NFC,
+    Ndef
   ]
 })
 export class AppModule {}
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { NFC, Ndef } from '@ionic-native/nfc';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private nfc: NFC, private ndef: Ndef) {
 
+  }
+
+  pagar(){
+    this.nfc.addNdefListener(() => {
+      console.log('successfully attached ndef listener');
+    }, (err) => {
+      console.log('error attaching ndef listener', err);
+    }).subscribe((event) => {
+      console.log('received ndef message. the tag contains: ', event.tag);
+      console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
+    
+      let message = this.ndef.textRecord('Hello world');
+      this.nfc.share([message]).then(onSuccess).catch(onError);
+    });
   }
 
 }
