f1f10f8d0f4a7c83f8355017d26533fe1f170a18
CalebeRios <calebe_rios@hotmail.com>
2018-02-01 18:36:13 -0200

    Adding dependencies for cordova and ionic

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -14,17 +14,6 @@ export class AboutPage {
   }
 
   pagar(){
-    this.nfc.addNdefListener(() => {
-      console.log('successfully attached ndef listener');
-    }, (err) => {
-      console.log('error attaching ndef listener', err);
-    }).subscribe((event) => {
-      console.log('received ndef message. the tag contains: ', event.tag);
-      console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
-    
-      let message = this.ndef.textRecord('Hello world');
-      this.nfc.share([message]).then(onSuccess).catch(onError);
-    });
   }
 
 }
